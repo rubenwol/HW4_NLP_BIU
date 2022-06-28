238e78800899cb650b4404246791eae7abb32b24
rubenwol <rwolhandler@gmail.com>
2022-06-28 10:25:33 +0300

    update some bug

--- model.py
+++ model.py
@@ -6,11 +6,12 @@ E1 = '[$]'
 E2 = '[#]'
 CLS = '[CLS]'
 
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
 
 class BertForRelationExtraction(nn.Module):
-    def __init__(self, model_path='bert-base-uncased', tokenizer_path='bert-base-uncased', outputs_dim=2, p=0.1):
+    def __init__(self, model_path='bert-base-uncased', outputs_dim=2, p=0.1):
         super(BertForRelationExtraction, self).__init__()
         self.bert_model = BertModel.from_pretrained(model_path)
         self.tanh = nn.Tanh()
@@ -21,15 +22,24 @@ class BertForRelationExtraction(nn.Module):
 
     def forward(self, inputs, e1_spans, e2_spans):
         out = self.bert_model(**inputs)
-        # need to change here !!!!
-        e1_avg = torch.mean(out[e1_spans[0]: e1_spans[1]], dim=1)
-        e2_avg = torch.mean(out[e2_spans[0]: e2_spans[1]], dim=1)
-        out1 = self.cls_linear(self.tanh(self.dropout(out[0])))
+        last_hidden_state = out.last_hidden_state
+        cls_hidden_state = out.pooler_output
+        e1_avg = self.avg_e_index(last_hidden_state, e1_spans)
+        e2_avg = self.avg_e_index(last_hidden_state, e2_spans)
+        out1 = self.cls_linear(self.tanh(self.dropout(cls_hidden_state)))
         out2 = self.entity_linear(self.tanh(self.dropout(e1_avg)))
         out3 = self.entity_linear(self.tanh(self.dropout(e2_avg)))
         out = torch.cat((out1, out2, out3), dim=0)
         out = self.final_linear(self.dropout(out))
         return F.sigmoid(out)
+
+    def avg_e_index(self, t, e_spans):
+        avg = torch.zeros((t.size(0), t.size(2)), device=device)
+        for i, (batch, e_span) in enumerate(zip(t, e_spans)):
+            range_ = torch.arange(e_span[0], e_span[1])
+            avg[i] = batch[range_].mean(dim=0)
+        return avg
+
 
 
 
